22e015c61674b56cab5e9bd71bdaed187014d279
Imai <imai@genio.co.jp>
2022-08-16 08:22:17 +0900

    first commit

---
+++ application/controllers/Login.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+
+class Login extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->database();
+        $this->load->library('session');
+        $this->load->model('login_model');
+    }
+
+
+    public function index()
+    {
+        echo "working..";
+    // $this->load->view("backend/login");
+    }
+
+    public function check_login()
+    {
+        $this->login_model->adminLoginFunction();
+        $this->session->set_flashdata("flash_message", $get_fhrase("Succsessfully logged in"));
+            redirect(base_url()."admin/dashboard", "refresh");
+    }
+}
---
+++ application/models/Login_model.php
@@ -0,0 +1,33 @@
+<?php
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+class Login extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function adminLoginFunction()
+    {
+        $email = html_escape($this->input->post("email"));
+        $password = html_escape($this->input->post("password"));
+        // Caution! sha1() is not secure
+        // $data = array("email" => $email,"password" => sha1($password));
+        $data = array("email" => $email,"password" => $password);
+        $query = $this->db->get_where("admin", $data);
+        if ($query->num_rows() > 0) {
+            $row = $query->row();
+            $this->session->set_userdata("login_type", "admin");
+            $this->session->set_userdata("admin_login", "1");
+            $this->session->set_userdata("admin_id", $row->admin_id);
+            $this->session->set_userdata("login_user_id", $row->admin_id);
+            $this->session->set_userdata("name", $row->name);
+        }elseif($query->num_rows() == 0){
+            $this->session->set_flashdata("error_message", $get_fhrase("missed logegd in"));
+            redirect(base_url()."login", "refresh");
+        }
+    }
+}
